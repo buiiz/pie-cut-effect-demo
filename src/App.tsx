44b9e4ca39c17de6df74d138c845fb2cba3f5551
Ivan Tur <ivantur2@gmail.com>
2022-12-07 12:12:20 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => (
+  <div className="app">
+    <div className="segment-1"></div>
+    <div className="segment-2"></div>
+    <div className="segment-3"></div>
+    <div className="center"></div>
+  </div>
+);
+
+export default App;
